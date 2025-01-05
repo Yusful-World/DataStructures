4c6dd5249c1911cb4f913404cfe583d65b73b0ec
Abdullahi Yusuf <abdullahiyusuf182@gmail.com>
2025-01-05 22:23:17 +0100

    An array class was created with a print method.

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -2,9 +2,29 @@
 
 namespace DataStructures
 {
-
-    public class Program(string[] args)
+    public class Array
     {
+        private readonly int[] items;
+        private int count;
+        public Array(int length)
+        {
+            items = new int[length];
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < count; i++)
+                Console.WriteLine(items[i]);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Array numbers = new Array(5);
+            numbers.Print();
+        }
 
     }
 }
